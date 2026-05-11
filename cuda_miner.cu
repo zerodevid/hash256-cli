b2f7684502e46c0019854f7dfb8e840690bb6d3d
Zero Dev <akbarabdul80@gmail.com>
2026-05-11 14:38:27 +0700

    refactor: update keccak implementation to use explicit byte buffers for accurate Solidity memory layout matching

--- cuda_miner.cu
+++ cuda_miner.cu
@@ -7,7 +7,6 @@
 typedef unsigned char uint8_t;
 typedef unsigned long long uint64_t;
 
-// Keccak-f[1600] constants
 __constant__ uint64_t d_keccakf_rndc[24] = {
     0x0000000000000001, 0x0000000000008082, 0x800000000000808a, 0x8000000080008000,
     0x000000000000808b, 0x0000000080000001, 0x8000000080008081, 0x8000000000008009,
@@ -18,15 +17,6 @@ __constant__ uint64_t d_keccakf_rndc[24] = {
 };
 
 #define ROL64(a, offset) ((a << offset) ^ (a >> (64 - offset)))
-
-// Byte swap for uint64_t to handle Big-Endian conversion
-__device__ inline uint64_t bswap64(uint64_t val) {
-    return __byte_perm(val, 0, 0x0123) | (__byte_perm(val >> 32, 0, 0x0123) << 32);
-}
-// Correct byte swap using intrinsic
-__device__ inline uint64_t swap64(uint64_t x) {
-    return ((x << 56) | ((x << 40) & 0xff000000000000ULL) | ((x << 24) & 0xff0000000000ULL) | ((x << 8) & 0xff00000000ULL) | ((x >> 8) & 0xff000000ULL) | ((x >> 24) & 0xff0000ULL) | ((x >> 40) & 0xff00ULL) | (x >> 56));
-}
 
 __device__ void keccakf(uint64_t s[25]) {
     int i, j, round;
@@ -44,7 +34,6 @@ __device__ void keccakf(uint64_t s[25]) {
         s[4] = ROL64(s[24], 14); s[24] = ROL64(s[21], 2); s[21] = ROL64(s[8], 55); s[8] = ROL64(s[16], 45);
         s[16] = ROL64(s[5], 36); s[5] = ROL64(s[3], 28); s[3] = ROL64(s[18], 21); s[18] = ROL64(s[17], 15);
         s[17] = ROL64(s[11], 10); s[11] = ROL64(s[7], 7); s[7] = ROL64(s[10], 3); s[10] = t;
-
         for (j = 0; j < 25; j += 5) {
             for (i = 0; i < 5; i++) bc[i] = s[j + i];
             for (i = 0; i < 5; i++) s[j + i] ^= (~bc[(i + 1) % 5]) & bc[(i + 2) % 5];
@@ -58,52 +47,49 @@ __global__ void mine_kernel(uint8_t *challenge, uint8_t *difficulty, uint64_t st
     uint64_t s[25];
     for (int i = 0; i < 25; i++) s[i] = 0;
 
-    // Challenge (32 bytes) - Copy directly as 64-bit lanes (Little-Endian to Little-Endian XOR)
-    uint64_t *challenge64 = (uint64_t*)challenge;
-    s[0] ^= challenge64[0];
-    s[1] ^= challenge64[1];
-    s[2] ^= challenge64[2];
-    s[3] ^= challenge64[3];
+    // Use a byte buffer for exact layout matching Solidity
+    uint8_t msg[136];
+    for (int i = 0; i < 136; i++) msg[i] = 0;
 
-    // Nonce (32 bytes uint256 in Solidity)
-    // Solidity: abi.encode(challenge, nonce) -> challenge[32] || nonce[32] (Big-Endian)
-    // Lane 4, 5, 6 will be 0 for a uint64 nonce
-    // Lane 7 gets the big-endian nonce
-    s[7] ^= swap64(nonce);
+    // Bytes 0-31: Challenge
+    for (int i = 0; i < 32; i++) msg[i] = challenge[i];
 
-    // Padding for Keccak-256 (64 bytes input -> 136 bytes rate)
-    s[8] ^= 0x01; 
-    s[16] ^= 0x8000000000000000ULL;
+    // Bytes 32-63: Nonce (uint256 Big-Endian)
+    // We populate only the last 8 bytes since our nonce is uint64
+    msg[63] = (uint8_t)(nonce & 0xFF);
+    msg[62] = (uint8_t)((nonce >> 8) & 0xFF);
+    msg[61] = (uint8_t)((nonce >> 16) & 0xFF);
+    msg[60] = (uint8_t)((nonce >> 24) & 0xFF);
+    msg[59] = (uint8_t)((nonce >> 32) & 0xFF);
+    msg[58] = (uint8_t)((nonce >> 40) & 0xFF);
+    msg[57] = (uint8_t)((nonce >> 48) & 0xFF);
+    msg[56] = (uint8_t)((nonce >> 56) & 0xFF);
+
+    // Keccak-256 Padding
+    msg[64] = 0x01;
+    msg[135] = 0x80;
+
+    // XOR byte buffer into lanes
+    for (int i = 0; i < 17; i++) {
+        uint64_t lane = 0;
+        for (int j = 0; j < 8; j++) lane |= ((uint64_t)msg[i * 8 + j] << (j * 8));
+        s[i] ^= lane;
+    }
 
     keccakf(s);
 
-    // Final Hash check (Big-Endian result in s[0..3])
-    // Contract check: hash < difficulty (uint256 comparison)
-    uint64_t *diff64 = (uint64_t*)difficulty;
-    
-    // We need to compare s[0..3] as Big-Endian uint256
-    // To compare easily, we swap back to little-endian host order for comparison
-    uint64_t h0 = swap64(s[0]);
-    uint64_t h1 = swap64(s[1]);
-    uint64_t h2 = swap64(s[2]);
-    uint64_t h3 = swap64(s[3]);
+    // Extract result hash bytes
+    uint8_t hash[32];
+    for (int i = 0; i < 4; i++) {
+        uint64_t lane = s[i];
+        for (int j = 0; j < 8; j++) hash[i * 8 + j] = (uint8_t)((lane >> (j * 8)) & 0xFF);
+    }
 
-    uint64_t d0 = swap64(diff64[0]);
-    uint64_t d1 = swap64(diff64[1]);
-    uint64_t d2 = swap64(diff64[2]);
-    uint64_t d3 = swap64(diff64[3]);
-
-    // Comparison of 256-bit Big-Endian integers (Most significant word first)
+    // Compare hash < difficulty (Big-Endian 256-bit)
     bool isLess = false;
-    if (h0 < d0) isLess = true;
-    else if (h0 == d0) {
-        if (h1 < d1) isLess = true;
-        else if (h1 == d1) {
-            if (h2 < d2) isLess = true;
-            else if (h2 == d2) {
-                if (h3 < d3) isLess = true;
-            }
-        }
+    for (int i = 0; i < 32; i++) {
+        if (hash[i] < difficulty[i]) { isLess = true; break; }
+        if (hash[i] > difficulty[i]) { break; }
     }
 
     if (isLess) {
@@ -115,50 +101,38 @@ __global__ void mine_kernel(uint8_t *challenge, uint8_t *difficulty, uint64_t st
 
 int main(int argc, char **argv) {
     if (argc < 4) return 1;
-    
     std::string challenge_hex = argv[1];
     std::string difficulty_hex = argv[2];
     uint64_t start_nonce = std::stoull(argv[3]);
-
     uint8_t h_challenge[32], h_difficulty[32];
     for (int i = 0; i < 32; i++) {
-        h_challenge[i] = std::stoi(challenge_hex.substr(i*2, 2), nullptr, 16);
-        h_difficulty[i] = std::stoi(difficulty_hex.substr(i*2, 2), nullptr, 16);
+        h_challenge[i] = std::stoi(challenge_hex.substr(i * 2, 2), nullptr, 16);
+        h_difficulty[i] = std::stoi(difficulty_hex.substr(i * 2, 2), nullptr, 16);
     }
-
     uint8_t *d_challenge, *d_difficulty;
-    uint64_t *d_found_nonce, h_found_nonce;
+    uint64_t *d_found_nonce;
     int *d_found_flag, h_found_flag = 0;
-
     cudaMalloc(&d_challenge, 32);
     cudaMalloc(&d_difficulty, 32);
     cudaMalloc(&d_found_nonce, sizeof(uint64_t));
     cudaMalloc(&d_found_flag, sizeof(int));
-
     cudaMemcpy(d_challenge, h_challenge, 32, cudaMemcpyHostToDevice);
     cudaMemcpy(d_difficulty, h_difficulty, 32, cudaMemcpyHostToDevice);
     cudaMemset(d_found_flag, 0, sizeof(int));
-
     int threads = 256;
-    int blocks = 1024 * 64; 
-
+    int blocks = 1024 * 64;
     while (true) {
         mine_kernel<<<blocks, threads>>>(d_challenge, d_difficulty, start_nonce, d_found_nonce, d_found_flag);
         cudaDeviceSynchronize();
-        
         cudaMemcpy(&h_found_flag, d_found_flag, sizeof(int), cudaMemcpyDeviceToHost);
         if (h_found_flag) {
+            uint64_t h_found_nonce;
             cudaMemcpy(&h_found_nonce, d_found_nonce, sizeof(uint64_t), cudaMemcpyDeviceToHost);
             std::cout << h_found_nonce << std::endl;
             break;
         }
         start_nonce += (uint64_t)blocks * threads;
     }
-
-    cudaFree(d_challenge);
-    cudaFree(d_difficulty);
-    cudaFree(d_found_nonce);
-    cudaFree(d_found_flag);
-
+    cudaFree(d_challenge); cudaFree(d_difficulty); cudaFree(d_found_nonce); cudaFree(d_found_flag);
     return 0;
 }
